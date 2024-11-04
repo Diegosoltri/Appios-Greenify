c752e5dda6a9a74464715002a177c13ce3caa5c3
JOSEPABLOGH <jpgalindoh@gmail.com>
2024-11-04 11:32:29 -0600

    Perfil Usuario

---
+++ Greenify/PerfilViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PerfilViewController.swift
+//  Greenify
+//
+//  Created by Jose Galindo on 04/11/24.
+//
+
+import UIKit
+
+class PerfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
