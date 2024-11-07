f0636fdd23e9bf40d4c1216c2ff3b52cd8d2c9fe
Diego <diegosoltri9@gmail.com>
2024-11-06 19:33:00 -0600

    cambios en home v1

--- Greenify/ImageActividades.swift
+++ Greenify/ImageActividades.swift
@@ -5,4 +5,19 @@
 //  Created by Diego Soltri on 04/11/24.
 //
 
-import Foundation
+import UIKit
+
+struct Imageactividades{
+    let tittle: String
+    let image: UIImage
+}
+
+let imageactividades: [Imageactividades] = [
+    Imageactividades(tittle: "Usa la bicicleta", image: #imageLiteral(resourceName: "bici")),
+    Imageactividades(tittle: "Riega el jardin TEC", image: #imageLiteral(resourceName: "jardin")),
+    Imageactividades(tittle: "Asiste al centro de reciclado", image: #imageLiteral(resourceName: "reci")),
+    Imageactividades(tittle: "Apoya en la camapaña de cuidado del agua", image: #imageLiteral(resourceName: "llave")),
+    Imageactividades(tittle: "Riega el jardin TEC", image: #imageLiteral(resourceName: "jardin")),
+    Imageactividades(tittle: "Riega el jardin TEC", image: #imageLiteral(resourceName: "jardin")),
+    Imageactividades(tittle: "Riega el jardin TEC", image: #imageLiteral(resourceName: "jardin")),
+]
--- Greenify/MainCollectionViewCell.swift
+++ Greenify/MainCollectionViewCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class MainCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    func setup(with imageactividades: Imageactividades){
+        mainImageView.image = imageactividades.image
+        mainLabel.text = imageactividades.tittle
+    }
+    
 }
