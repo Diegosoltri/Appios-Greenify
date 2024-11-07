308b22f11d2774cfb26d78d5a9c4ab698f13a9e4
Diego <diegosoltri9@gmail.com>
2024-11-06 19:32:37 -0600

    cambios en home

---
+++ Greenify/ImageActividades.swift
@@ -0,0 +1,8 @@
+//
+//  ImageActividades.swift
+//  Greenify
+//
+//  Created by Diego Soltri on 04/11/24.
+//
+
+import Foundation
---
+++ Greenify/MainCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MainCollectionViewCell.swift
+//  Greenify
+//
+//  Created by Diego Soltri on 04/11/24.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+}
